c86599f3579d7efd4ceb52b24e83db14f568af3a
Afnur Reja <afnurreja884@gmail.com>
2026-07-02 17:05:27 +0530

    update server.js

--- 10_Networking_with_core_nodejs_module/05_creating-tcp-server/server.js
+++ 10_Networking_with_core_nodejs_module/05_creating-tcp-server/server.js
@@ -2,20 +2,26 @@ import net from "node:net";
 
 const server = net.createServer();
 
+// starting/Listening server
 server.listen(4000, "0.0.0.0", () => {
   console.log("Listening on port 4000");
 });
 
+
+// When a client connect with the server, "connection" event emits on server.
+// Inside the callback we get socket (duplex stream).
 server.on("connection", (socket) => {
   socket.on("data", (chunk) => {
-    // console.log(chunk.toString());
+    console.log(chunk.toString());
+    socket.write("HTTP\n\nGot your message.")
+    socket.end()
   });
 
   socket.on("close", () => {
     console.log("Client Disconnected : ", socket.remoteAddress);
   });
 
-//   console.log(socket.address());
+//   console.log(socket.address());    // Server Address.
   console.log("Client Connected : ", socket.remoteAddress);
 //   console.log(socket.remoteFamily);
 //   console.log(socket.remoteAddress);
