673250e4eae728f408cc8eb64ee9a51933d3b35f
Afnur Reja <afnurreja884@gmail.com>
2026-07-06 20:26:52 +0530

    learn transfer files

---
+++ 10_Networking_with_core_nodejs_module/08_transferring-files-using-tcp/client.js
@@ -0,0 +1,31 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import net from "node:net";
+
+process.stdin.on("data", (input) => {
+  const inputStr = input.toString().trim();
+  if (inputStr === "send") {
+    const readStream = createReadStream("E:\\Iss_Barsaat_.mp4")
+    readStream.pipe(socket);
+    readStream.on("end", () => {
+        console.log("file ended.");
+    })
+  }
+});
+
+const socket = net.createConnection({ host: "10.61.154.69", port: 4000 });
+
+// const writeStream = createWriteStream("C:\\Users\\afnur\\OneDrive\\Desktop\\song.mp4")
+// socket.pipe(writeStream);
+
+// socket.on("data", (chunk) => {
+//     console.log(chunk.toString());
+// })
+
+socket.on("error", () => {
+  console.log("Server Lost.");
+});
+
+// setTimeout(() => {
+//     socket.write("Hello, from client.js")
+//     // socket.end()
+// }, 3000);
---
+++ 10_Networking_with_core_nodejs_module/08_transferring-files-using-tcp/server.js
@@ -0,0 +1,54 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import net from "node:net";
+
+// process.stdin.on("data", (input) => {
+//    const inputStr = input.toString().split(" ")
+//    const [clientIndex] = inputStr
+//    if(!Number.isNaN(Number(clientIndex))) {
+//       clientsList[parseInt(clientIndex)].write(inputStr.slice(1).join(" "))
+//    } else {
+//       clientsList.forEach((clientSocket) => {
+//          clientSocket.write(input);
+//       })
+//    }
+// })
+
+const clientsList = []
+
+const server = net.createServer((socket) => {
+  console.log("Client Connected : ", socket.remoteAddress);``
+  clientsList.push(socket)
+
+  const writeStream = createWriteStream("song.mp4");
+  socket.pipe(writeStream);
+
+  // const readStream = createReadStream("song.mp4");
+  // readStream.pipe(socket);
+
+  // socket.on("data", (chunk) => {
+  //   console.log(chunk.toString());
+  //   // socket.write("Got your message.");
+
+  //   const id = clientsList.indexOf(socket)
+  //   clientsList.forEach((clientSocket) => {
+  //   if(clientSocket !== socket) clientSocket.write(`Client ${id} : ${chunk}`);
+
+  //  })
+  // });
+
+  socket.on("close", () => {
+    console.log("Client Disconnected : ", socket.remoteAddress);
+  });
+
+  socket.on("error", () => {
+    console.log("Client Lost.");
+  });
+});
+
+server.listen(4000, "0.0.0.0", () => {
+  console.log("Server started on port 4000");
+});
+
+
+
+// Task : upload a file by a client than sent it to each client connected to the server except that client.
